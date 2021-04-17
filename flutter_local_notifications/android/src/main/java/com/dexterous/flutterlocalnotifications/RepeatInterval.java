4f6c5c10d8b8c988f7057ab0ccfa8e72d012cab4
Omar Ihab <54323091+Xellly@users.noreply.github.com>
2021-04-17 13:26:35 +0300

    Update RepeatInterval.java

--- flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/RepeatInterval.java
+++ flutter_local_notifications/android/src/main/java/com/dexterous/flutterlocalnotifications/RepeatInterval.java
@@ -5,7 +5,10 @@ import androidx.annotation.Keep;
 @Keep
 public enum RepeatInterval {
     EveryMinute,
+    EveryFiveMinutes,
+    EveryThirtyMinutes,
     Hourly,
+    EveryTwoHours,
     Daily,
     Weekly
 }
